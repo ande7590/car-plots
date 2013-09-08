5844da01529d51201ac533eace31d0a559edc296
Mike <mr.anderson.1725@gmail.com>
2013-09-07 21:58:04 -0500

    analysis template

---
+++ carplots-analysis/src/main/R/analysisTemplate.R
@@ -0,0 +1,4 @@
+carplotsService <- new(serviceClassName)
+carplots.makeModels <- getMakeModels(carplotsService)
+carplots.locations <- getSearchLocations(carplotsService)
+carplots.scraperRuns <- getScraperRuns(carplotsService)
